b9dfb604db8ed941142618689a5216e4ebc105be
JINNYLOG <149363095+JINNYLOG@users.noreply.github.com>
2024-10-23 10:17:54 +0900

    Create Weather Observation Station 10.sql

---
+++ SQL/Weather Observation Station 10.sql
@@ -0,0 +1,6 @@
+# Query the list of CITY names from STATION that do not end with vowels. 
+# Your result cannot contain duplicates.
+
+SELECT distinct CITY
+FROM STATION
+WHERE CITY NOT REGEXP '[aeiou]$' ;
